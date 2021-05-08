a5dc4d3dc90de82679b962cb9eb4a958c69701b5
Brayden Brown <braydenbrown@gmail.com>
2021-05-08 15:58:50 -0700

    Change function declaration to remove un-needed comments

---
+++ main.rb
@@ -0,0 +1,7 @@
+def draw_dark_or_light_button(label_text, x, y, foreground_color, is_dark_mode)
+  if is_dark_mode
+    paint(label_text, x, y, foreground_color - 10, '#111111')
+  else
+    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+  end
+end
