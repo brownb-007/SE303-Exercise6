eadc3680714f68779f1a524822f5492eae01c171
Brayden Brown <braydenbrown@gmail.com>
2021-05-08 17:11:23 -0700

    Combine functions into a class(144) and Move Functions(198)

--- main.rb
+++ main.rb
@@ -1,11 +1,3 @@
-def draw_button_dark_mode(label_text, x, y, foreground_color)
-  paint(button.label_text, button.x, button.y, button.foreground_color - 10,  '#111111')
-end
-
-def draw_button_light_mode(label_text, x, y, foreground_color)
-  paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
-end
-
 class Button
   attr_reader :label_text, :x, :y, :foreground_color
   def intialize(label_text, x, y, foreground_color)
@@ -13,5 +5,13 @@ class Button
     @x = x
     @y = y
     @foreground_color = foreground_color
+  end
+
+  def draw_button_dark_mode(label_text, x, y, foreground_color)
+    paint(label_text, x, y, foreground_color - 10,  '#111111')
+  end
+  
+  def draw_button_light_mode(label_text, x, y, foreground_color)
+    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
   end
 end
